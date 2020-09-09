e9f565c4af2ce9c4129c74ca229602552eb5ae2b
Annel Cota <acch1010@cs.unm.edu>
2020-09-09 14:52:10 -0600

    create domino class

---
+++ src/Domino.java
@@ -0,0 +1,17 @@
+public class Domino {
+    private int leftNum;
+    private int rightNum;
+
+    public Domino(int leftNum, int rightNum) {
+        this.leftNum = leftNum;
+        this. rightNum = rightNum;
+    }
+
+    public int getLeftNum() {
+        return leftNum;
+    }
+
+    public int getRightNum() {
+        return rightNum;
+    }
+}
