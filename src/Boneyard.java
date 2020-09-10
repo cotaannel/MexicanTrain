809a4a3d5050a4e322bff79aed6ef5580ad66c78
Annel Cota <acch1010@cs.unm.edu>
2020-09-10 16:22:03 -0600

    create boneyard with dominos

--- src/Boneyard.java
+++ src/Boneyard.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Boneyard {
+    public ArrayList<Domino> boneyard;
+
+    public Boneyard() {
+        boneyard = new ArrayList<Domino>();
+        createBoneyard();
+    }
+
+    public void createBoneyard() {
+        for(int i = 0; i <= 9; i++) {
+            int j = i;
+            while (j >= 0) {
+                boneyard.add(new Domino(i,j));
+                j--;
+            }
+        }
+        Collections.shuffle(boneyard);
+    }
+
+
 }
